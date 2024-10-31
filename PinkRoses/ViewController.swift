129246c5e6187a46fa22aff0869ddb711e418829
wooseng <zhanbaocheng@vip.qq.com>
2024-10-31 21:56:58 +0800

    Initial Commit

---
+++ PinkRoses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinkRoses
+//
+//  Created by 詹保成 on 2024/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PinkRosesTests/PinkRosesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PinkRosesTests.swift
+//  PinkRosesTests
+//
+//  Created by 詹保成 on 2024/10/31.
+//
+
+import Testing
+@testable import PinkRoses
+
+struct PinkRosesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
